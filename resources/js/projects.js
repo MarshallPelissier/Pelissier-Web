e2d5e1b9f0627107547a5577d53d735f9178058f
MarshallPelissier <pelissierm@spu.edu>
2016-06-02 15:48:11 -0700

    created projects page

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -5,11 +5,25 @@ $(document).ready(function() {
     $('.info').not($Elem).removeClass('info-open');
     if($('.info').hasClass('info-open')){
     	$('.content').removeClass('content-open');
+      $('.info').css("-webkit-transition","height .25s, border 0s linear .21s, background-color .25s linear");
+      $('.info').css("-moz-transition","height .25s, border 0s linear .21s, background-color .25s linear");
+      $('.info').css("-o-transition","height .25s, border 0s linear .21s, background-color .25s linear");
+      $('.info').css("transition","height .25s, border 0s linear .21s, background-color .25s linear");
     }
     else{
+      $('.info').css("-webkit-transition","height .25s, border 0s linear 0s, background-color .25s linear");
+      $('.info').css("-moz-transition","height .25s, border 0s linear 0s, background-color .25s linear");
+      $('.info').css("-o-transition","height .25s, border 0s linear 0s, background-color .25s linear");
+      $('.info').css("transition","height .25s, border 0s linear 0s, background-color .25s linear");
     	$('.content').addClass('content-open');
     }
   	$(this).toggleClass('icon-box-open');
     $Elem.toggleClass('info-open');
+    if($('.info').hasClass('info-open')){
+      $('.info').not($Elem).css("-webkit-transition","height .25s, border 0s linear .21s, background-color .25s linear");
+      $('.info').not($Elem).css("-moz-transition","height .25s, border 0s linear .21s, background-color .25s linear");
+      $('.info').not($Elem).css("-o-transition","height .25s, border 0s linear .21s, background-color .25s linear");
+      $('.info').not($Elem).css("transition","height .25s, border 0s linear .21s, background-color .25s linear");
+    }
   });
 });
