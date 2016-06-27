c8fa775bc53ddb5ff625314b7b2225879385bc9d
MarshallPelissier <pelissierm@spu.edu>
2016-06-27 13:08:31 -0700

    Fix #1 Added media queries so mobile view is called when the viewport is the correct size. Fixed dimensions of some elements on the main page, Changed info text color in dark mode, Home links dont have /index.html anymore

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
   var icon_line_height;
   var icon_line_height_selected;
   var icon_line_height_small;
+
+  var mobile_clicked = false;
+
   function icon_css(){
     icon_height = '12vmin';
     icon_height_small = '7vmin';
@@ -13,6 +16,7 @@ $(document).ready(function() {
     icon_line_height_selected = '6vmin';
     icon_line_height_small = '7.5vmin';
   };
+
   function icon_css_mobile(){
     icon_height = '14vmin';
     icon_height_small = '10vmin';
@@ -20,41 +24,90 @@ $(document).ready(function() {
     icon_line_height_selected = '9vmin';
     icon_line_height_small = '11vmin';
   };
+
   function icon_css_games_mobile(){
     icon_height = '12vmin';
     icon_line_height = '12vmin';
   }
+
+  function mobile_view(){
+    $('*').toggleClass('mobile');
+
+    if ($('*').hasClass('mobile')){
+      icon_css_mobile();
+      if($(document).find('title').text() === 'Games'){
+        icon_css_games_mobile();
+      }
+    }
+    else{
+      icon_css();
+    }
+    if($('.info').hasClass('info-open')){
+      $('.icon-box').css('height',icon_height_small);
+      $('.icon-box').css('border-radius','0px');
+      $('.icon-box').css('line-height',icon_line_height_small);
+      $('.icon-box-open').css('line-height',icon_line_height_selected);
+    }
+    else{
+      $('.icon-box').css('height',icon_height);
+      $('.icon-box').css('border-radius','5px');
+      $('.icon-box').css('line-height',icon_line_height);
+    }
+  };
+
+  function dark_view(){
+      $('*').toggleClass('dark');
+  };
+
+  function check_mobile(){
+    //change to mobile view based on media query
+    if (($('.buffer').css('float') == 'right') && !($('*').hasClass('mobile')) ){
+      mobile_view();
+    }
+    //doesnt allow change back if mobile button has been clicked
+    else if (!($('.buffer').css('float') == 'right') && ($('*').hasClass('mobile')) && !mobile_clicked){
+      mobile_view();
+    }
+    //allows for mobile when the size isnt correct but the button has been pressed;
+    else if (!($('.buffer').css('float') == 'right') && !($('*').hasClass('mobile')) && mobile_clicked){
+      mobile_view();
+    }
+  };
+
+  check_mobile();
+  $(window).resize(check_mobile);
+
   $('.icon-box').click(function(){
 
     //transition timing functions
     // opening is instant
     // closing is delayed
     function this_delay(){
-      $Elem.css("-webkit-transition","height .25s, border 0s linear .21s, background-color .25s linear, padding .15s");
-      $Elem.css("-moz-transition","height .25s, border 0s linear .21s, background-color .25s linear, padding .15s");
-      $Elem.css("-o-transition","height .25s, border 0s linear .21s, background-color .25s linear, padding .15s");
-      $Elem.css("transition","height .25s, border 0s linear .21s, background-color .25s linear, padding .15s");
+      $Elem.css('-webkit-transition','height .25s, border 0s linear .21s, padding .15s');
+      $Elem.css('-moz-transition','height .25s, border 0s linear .21s, padding .15s');
+      $Elem.css('-o-transition','height .25s, border 0s linear .21s, padding .15s');
+      $Elem.css('transition','height .25s, border 0s linear .21s, padding .15s');
     }
     function this_instant(){
-      $Elem.css("-webkit-transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
-      $Elem.css("-moz-transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
-      $Elem.css("-o-transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
-      $Elem.css("transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
+      $Elem.css('-webkit-transition','height .25s, border 0s linear .04s, padding .15s');
+      $Elem.css('-moz-transition','height .25s, border 0s linear .04s, padding .15s');
+      $Elem.css('-o-transition','height .25s, border 0s linear .04s, padding .15s');
+      $Elem.css('transition','height .25s, border 0s linear .04s, padding .15s');
     }
     function others_delay(){
-      $('.info').not($Elem).css("-webkit-transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
-      $('.info').not($Elem).css("-moz-transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
-      $('.info').not($Elem).css("-o-transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15s");
-      $('.info').not($Elem).css("transition","height .25s, border 0s linear .04s, background-color .25s linear, padding .15ss");
+      $('.info').not($Elem).css('-webkit-transition','height .25s, border 0s linear .04s, padding .15s');
+      $('.info').not($Elem).css('-moz-transition','height .25s, border 0s linear .04s, padding .15s');
+      $('.info').not($Elem).css('-o-transition','height .25s, border 0s linear .04s, padding .15s');
+      $('.info').not($Elem).css('transition','height .25s, border 0s linear .04s, padding .15ss');
     }
     //variables. id (TN-#), info being opened, shortend icon text, full icon text.
     var id = $(this).attr('id');
-    var $Elem = $('#' + id + "-info");
-    var $short = $('#' + id + "-short");
-    var $full = $('#' + id + "-full");
+    var $Elem = $('#' + id + '-info');
+    var $short = $('#' + id + '-short');
+    var $full = $('#' + id + '-full');
     if ($('*').hasClass('mobile')){
       icon_css_mobile();
-      if($(document).find('title').text() === "Games"){
+      if($(document).find('title').text() === 'Games'){
         icon_css_games_mobile();
       }
     }
@@ -68,18 +121,18 @@ $(document).ready(function() {
     $('.full-text').not($full).hide();
     if($('.info').hasClass('info-open')){
     	$('.content').removeClass('content-open');
-      $('.icon-box').css("height",icon_height);
-      $('.icon-box').css("border-radius","5px");
-      $('.icon-box').css("line-height",icon_line_height);
+      $('.icon-box').css('height',icon_height);
+      $('.icon-box').css('border-radius','5px');
+      $('.icon-box').css('line-height',icon_line_height);
 
       //transition timings. delay this element
       this_delay();
     }
     else{
-      $('.icon-box').css("height",icon_height_small);
-      $('.icon-box').css("border-radius","0px");
-      $('.icon-box').not($(this)).css("line-height",icon_line_height_small);
-      $(this).css("line-height",icon_line_height_selected);
+      $('.icon-box').css('height',icon_height_small);
+      $('.icon-box').css('border-radius','0px');
+      $('.icon-box').not($(this)).css('line-height',icon_line_height_small);
+      $(this).css('line-height',icon_line_height_selected);
 
       //transition timings. instant transition for this element
       this_instant();
@@ -106,31 +159,17 @@ $(document).ready(function() {
 
   });
   $('.menu-dark').click(function(){
-    $('*').toggleClass('dark');
+    dark_view();
   });
   $('.menu-view').click(function(){
-    $('*').toggleClass('mobile');
-
-    if ($('*').hasClass('mobile')){
-      icon_css_mobile();
-      if($(document).find('title').text() === "Games"){
-        icon_css_games_mobile();
-      }
+    //toggle mobile_clicked
+    $(this).toggleClass('menu-botton-clicked');
+    if (mobile_clicked){
+      mobile_clicked = false
     }
     else{
-      icon_css();
+      mobile_clicked = true;
     }
-    if($('.info').hasClass('info-open')){
-      $('.icon-box').css("height",icon_height_small);
-      $('.icon-box').css("border-radius","0px");
-      $('.icon-box').css("line-height",icon_line_height_small);
-      $('.icon-box-open').css("line-height",icon_line_height_selected);
-    }
-    else{
-      $('.icon-box').css("height",icon_height);
-      $('.icon-box').css("border-radius","5px");
-      $('.icon-box').css("line-height",icon_line_height);
-    }
-
+    check_mobile();
   });
 });
